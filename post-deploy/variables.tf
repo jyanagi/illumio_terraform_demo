c1c45352fa5f9623e6b17b7a3d53cb0e9491ea69
Jeff Yanagi <75082831+jyanagi@users.noreply.github.com>
2025-06-18 17:50:09 -0400

    Update variables.tf

--- post-deploy/variables.tf
+++ post-deploy/variables.tf
@@ -1,4 +1,18 @@
+# AWS Variables
+
 variable "aws_region" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "ami_user" {
+  type = string
+}
+
+variable "instance_type" {
   type = string
 }
 
@@ -10,6 +24,21 @@ variable "private_key_path" {
   type = string
 }
 
-variable "ami_user" {
-    type = string
+# Illumio Variables
+
+variable "pce_url" {
+  type = string
 }
+
+variable "pce_org_id" {
+  type = string
+}
+
+variable "pce_api_key" {
+  type = string
+}
+
+variable "pce_api_secret" {
+  type = string
+}
+
