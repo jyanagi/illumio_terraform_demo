ee66da1503287ad5762c0e45bbb4bf2d019f5ab4
Jeff Yanagi <75082831+jyanagi@users.noreply.github.com>
2025-06-18 18:09:55 -0400

    Update helm_install.sh

--- aws/helm_install.sh
+++ aws/helm_install.sh
@@ -4,5 +4,8 @@
 exec > /var/log/helm-install.log 2>&1
 set -x
 
+# Export K3s flags before calling helm installer
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+
 # Download and install Helm
 curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
