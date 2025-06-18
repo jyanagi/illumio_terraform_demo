7bc25eb501a21827b3047a33e8dfd55a6216f18d
Jeff Yanagi <75082831+jyanagi@users.noreply.github.com>
2025-06-18 17:49:57 -0400

    Update variables.tf

--- illumio/variables.tf
+++ illumio/variables.tf
@@ -1,3 +1,31 @@
+# AWS Variables
+
+variable "aws_region" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "ami_user" {
+  type = string
+}
+
+variable "instance_type" {
+  type = string
+}
+
+variable "public_key_path" {
+  type = string
+}
+
+variable "private_key_path" {
+  type = string
+}
+
+# Illumio Variables
+
 variable "pce_url" {
   type = string
 }
