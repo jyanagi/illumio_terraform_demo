1b445bf98e3ae5d07dc77597360b1ddcd6eed4af
Jeff Yanagi <75082831+jyanagi@users.noreply.github.com>
2025-06-17 21:47:50 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,7 @@ terraform -chdir=illumio apply -auto-approve
 # Then deploy aws
 terraform -chdir=aws init
 terraform -chdir=aws apply -auto-approve
+
+# Then perform post-deployment activities
+terraform -chdir=post-deploy init
+terraform -chdir=post-deploy -auto-approve
