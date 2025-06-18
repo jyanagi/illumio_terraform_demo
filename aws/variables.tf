1dd4a4bdd69413e8465a8319fc7f8438ebd2308e
Jeff Yanagi <75082831+jyanagi@users.noreply.github.com>
2025-06-18 02:17:02 -0400

    Update variables.tf

--- aws/variables.tf
+++ aws/variables.tf
@@ -1,28 +1,23 @@
 variable "ami_id" {
-  default = "ami-053b0d53c279acc90" # Ubuntu 22.04
+  type = string
 }
 
 variable "instance_type" {
-  default = "t3.small"
+  type = string
 }
 
 variable "aws_region" {
-  #default = "us-east-1"
   type = string
 }
 
 variable "public_key_path" {
-  #default = "~/.ssh/id_rsa.pub"
   type = string
 }
 
 variable "private_key_path" {
-  #default = "~/.ssh/id_rsa"
   type = string
 }
 
 variable "ami_user" {
-    #default = "ec2-user"
-    #default = "ubuntu"
     type = string
 }
