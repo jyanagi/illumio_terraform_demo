361ccba3826b02b9540c0bdc5ce3dd408f474dd4
Jeff Yanagi <75082831+jyanagi@users.noreply.github.com>
2025-06-18 08:41:17 -0400

    Update kubelink_install.sh

--- post-deploy/kubelink_install.sh
+++ post-deploy/kubelink_install.sh
@@ -4,5 +4,9 @@ set -x
 
 export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 
+# Create Namespace and Configmap for Private PKI
+kubectl create namespace illumio-system
+kubectl -n illumio-system create configmap private-ca --from-file=/tmp/private-ca.crt
+
 # Install Illumio Kubelink Operator
-helm install illumio -f /tmp/illumio-values.yaml oci://quay.io/illumio/illumio --namespace illumio-system --create-namespace
+helm install illumio -f /tmp/illumio-values.yaml oci://quay.io/illumio/illumio --namespace illumio-system #--create-namespace
