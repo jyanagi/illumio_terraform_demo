c73ef390a12bb4281c720bcffffc37388ed708ab
Jeff Yanagi <75082831+jyanagi@users.noreply.github.com>
2025-06-17 23:39:42 -0400

    Delete aws directory

--- aws/helm_install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Enable Logging - refer to /var/log/user-data.log for k3s installation messages
-exec >> /var/log/user-data.log 2>&1
-set -x
-
-# Download and install Helm
-curl -fsSL https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
